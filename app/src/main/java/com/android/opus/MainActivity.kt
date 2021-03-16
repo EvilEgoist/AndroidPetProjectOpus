673eaf375bf6c75f24613e4b97395f98031ce6ed
sviridov.ivan <artem.sviridov.99@list.ru>
2021-03-16 20:02:08 +0300

    Part of screen 22

--- app/src/main/java/com/android/opus/MainActivity.kt
+++ app/src/main/java/com/android/opus/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.android.opus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.android.opus.ui.screen.MainScreenFragment
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, MainScreenFragment.newInstance()).commit()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .add(R.id.fragment_container, MainScreenFragment.newInstance()).commit()
+//        }
+//        Intent(this, SkillsScreen::class.java)
+
+        val textViewArray = arrayOfNulls<TextView>(10)
+        textViewArray[0]
+
     }
 }
---
+++ app/src/main/java/com/android/opus/SkillsScreen.kt
@@ -0,0 +1,15 @@
+package com.android.opus
+
+import android.R
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SkillsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_skill_screen)
+    }
+}
