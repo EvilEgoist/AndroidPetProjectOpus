80f5e1d938b7e9518b717227bcad6ae005982d33
RoundKubik <kubov.a.nikita@gmail.com>
2021-04-09 12:51:10 +0300

    [MINOR] - изменён ресайклер в макете

--- app/src/main/java/com/android/opus/MainNavigationActivity.kt
+++ app/src/main/java/com/android/opus/MainNavigationActivity.kt
@@ -19,7 +19,7 @@ class MainNavigationActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_container)
         bottomNav.setupWithNavController(navController)
 
-        setSupportActionBar(nav_title)
+        setSupportActionBar(toolbar)
         val appBarConfiguration = AppBarConfiguration(
             topLevelDestinationIds = setOf (
                 R.id.vacancyScreenFragment,
